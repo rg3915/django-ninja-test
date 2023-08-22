a42b13c25af1bcd68d59d7db1b21208a07c62107
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-08-22 10:25:42 -0300

    edit e delet

--- backend/core/static/js/crm/customer.js
+++ backend/core/static/js/crm/customer.js
@@ -1,31 +1,52 @@
-const url = '/api/v1/crm/customer/'
-const headers = {"Content-Type": "application/json"}
+const url = '/api/v1/crm/customer/';
+const headers = {"Content-Type": "application/json"};
 
 const getItems = () => ({
     filteredItems: [],
     editItem: {},
 
-    init(){
-        this.getData()
+    init() {
+        this.getData();
     },
 
-    getData(){
-        axios.get(url).then(response => { this.filteredItems = response.data })
+    getData() {
+        axios.get(url).then(response => {
+            this.filteredItems = response.data;
+        });
     },
 
-    resetForm(){ //limpar o campo
-        this.editItem={}
+    resetForm() {
+        this.editItem = {};
     },
 
-    saveData(){
-        if (!this.editItem){
-            return
+    saveData() {
+        if (!this.editItem.id) { // Verifique se é uma edição ou adição
+            axios.post(url, this.editItem, { headers: headers })
+                .then(response => {
+                    this.filteredItems = [response.data, ...this.filteredItems];
+                    this.resetForm();
+                });
+        } else {
+            axios.put(`${url}${this.editItem.id}/`, this.editItem, { headers: headers })
+                .then(response => {
+                    const index = this.filteredItems.findIndex(item => item.id === response.data.id);
+                    this.filteredItems[index] = response.data;
+                    this.resetForm();
+                });
         }
-        const bodyData = { ...this.editItem} //spread operator 
-        axios.post(url, bodyData, {headers: headers})
-            .then(response => {
-                this.filteredItems = [response.data, ...this.filteredItems]
-                this.resetForm()
-            })
     },
-})
+
+    editItem(item) {
+        this.editItem = { ...item }; // Copia os dados do item para o editItem
+    },
+
+    deleteItem(item) {
+        console.log(this.deleteItem);
+        axios.delete(`${url}${item.id}/`, { headers: headers })
+            .then(() => {
+                this.filteredItems = this.filteredItems.filter(i => i.id !== item.id);
+                this.resetForm();
+            });
+    },
+});
+
--- backend/crm/api.py
+++ backend/crm/api.py
@@ -22,3 +22,17 @@ def list_customer(request):
 @router.post('customer/', response={HTTPStatus.CREATED: CustomerSchema})
 def create_customer(request, payload: CustomerSchemaIn):
     return Customer.objects.create(**payload.dict())
+
+@router.put('customer/{customer_id}/', response=CustomerSchema)
+def update_customer(request, customer_id: int, payload: CustomerSchemaIn):
+    customer = Customer.objects.get(id=customer_id)
+    for key, value in payload.dict().items():
+        setattr(customer, key, value)
+    customer.save()
+    return customer
+
+@router.delete('customer/{customer_id}/', response={HTTPStatus.NO_CONTENT: None})
+def delete_customer(request, customer_id: int):
+    customer = Customer.objects.get(id=customer_id)
+    customer.delete()
+    return None
